1ea905283044b975054a6bba35de967293a3315e
ganesh-azgit <ganesh.1784@gmail.com>
2021-10-18 21:50:24 +0530

    run code 4

---
+++ var.tf
@@ -0,0 +1,20 @@
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+    }
+
+variable "image_id" {
+    type = string
+    default = "ami-0428fc1ee1bde045a"
+    }
+
+variable "tags" {
+    type = map
+    default = {
+        name = "ganesh2022"
+    }
+}
+
+variable "secret_key" {
+    type = string
+    }
